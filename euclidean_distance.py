8d3270dfd69eb0f1a4414f6047ef068a5c0fc9e4
Govind Kushwah <gk6647891@gmail.com>
2025-05-21 10:01:31 +0530

    Create euclidean_distance.py

---
+++ euclidean_distance.py
@@ -0,0 +1,6 @@
+x1=float(input("Enter value of x1 - "))
+y1=float(input("Enter value of y1 - "))
+x2=float(input("Enter value of x2 - "))
+y2=float(input("Enter value of y2 - "))
+euclidean_distance=((x2-x1)*2 +(y2-y1)2)*0.5
+print("Euclidean distance b/w",x1,",",y1,"and",x2,",",y2,"is",euclidean_distance)
