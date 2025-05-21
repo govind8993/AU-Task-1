a598023898cf623259ca4dc83e6ab58ff1290aa2
Govind Kushwah <gk6647891@gmail.com>
2025-05-21 10:26:23 +0530

    Create find_Nth_term.py

---
+++ find_Nth_term.py
@@ -0,0 +1,8 @@
+#Given the first 2 terms of an Arithmetic Series.Find the Nth term 
+#of the series. Assume all inputs are provided by the user.
+
+n1=4
+n2=7
+n=100
+nth_term=n1+(n-1)*(n2-n1)
+print("Nth term - ",nth_term)
